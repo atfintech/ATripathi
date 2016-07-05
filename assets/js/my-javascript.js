722f99b8cfdc9fa56f91b216e9116417bc65adf2
ceo12345 <tripathi@vesthq.com>
2016-07-04 22:02:44 -0500

    added test file

--- assets/js/my-javascript.js
+++ assets/js/my-javascript.js
@@ -2,16 +2,18 @@ $(function() {
 
 	window.setTimeout(function() {
 		$('.accordian').css('opacity', '1');
-	}, 2000);
+	}, 90);
 
 	$('div').addClass('default');
 
-		$('div').on('click', function() {
+	$('div').on('click', function() {
 
-	  	var e = $('.accordian > div');
-			if(e.hasClass('expand')){
-				 e.removeClass('expand');
-			 	$(this).addClass('expand');
-			} else { $(this).addClass('expand'); }
-		})
+		var e = $('.accordian > div');
+		if (e.hasClass('expand')) {
+			e.removeClass('expand');
+			$(this).addClass('expand');
+		} else {
+			$(this).addClass('expand');
+		}
+	})
 })
